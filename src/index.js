d7d58f7d6131cfd3a30aedbcafdd1ef301fe2020
T.Sanyi <s.truczkai@gmail.com>
2023-09-25 18:58:50 +0200

    clean code

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ root.appendChild(element) */ // this works, but it's not React
 
 // ReactDOMC.render(<h1>Hello everyone!</h1>, document.getElementById("root")) // .render is deprecated on ReactDOM
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<h1>Hello everyone!</h1>)
