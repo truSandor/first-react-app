366097e1d99be6f615e02da4b2e7a8110af11e7f
T.Sanyi <s.truczkai@gmail.com>
2023-09-25 18:37:03 +0200

    add 'hello world' code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
+
+/* const element = document.createElement("h1")
+element.innerText = "Hello, everyone!"
+root.appendChild(element) */ // this works, but it's not React
+
+// ReactDOMC.render(<h1>Hello everyone!</h1>, document.getElementById("root")) // .render is deprecated on ReactDOM
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<h1>Hello everyone!</h1>)
