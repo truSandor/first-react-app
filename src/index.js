d2f307216a20d2ab547ac62974863ae16f5780c7
T.Sanyi <s.truczkai@gmail.com>
2023-09-26 18:56:10 +0200

    add more content to the page

--- src/index.js
+++ src/index.js
@@ -8,4 +8,19 @@ root.appendChild(element) */ // this works, but it's not React
 // ReactDOMC.render(<h1>Hello everyone!</h1>, document.getElementById("root")) // .render is deprecated on ReactDOM
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<h1>Hello everyone!</h1>)
+
+const page = (
+  <div>
+      <img src="react-logo.png" alt="react logo" width="80px" />
+      <h1>Fun facts about React</h1>
+      <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+  </div>
+)
+
+root.render(page)
