3a6ab72b3da630512e391f696248d5332aa5281a
T.Sanyi <s.truczkai@gmail.com>
2023-09-26 19:15:03 +0200

    add more custom elements

--- src/index.js
+++ src/index.js
@@ -9,18 +9,39 @@ root.appendChild(element) */ // this works, but it's not React
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
+const Img = () => <img src="react-logo.png" alt="react logo" width="40px" />
+
+const Nav = () => (
+  <nav>
+    <Img />
+  </nav>
+)
+
+const Header = () => (
+  <header>
+    <Nav />
+  </header>
+)
+
+const Footer = () => (
+  <footer>
+    <small>© 2023 TruSandor development. No rights reserved.</small>
+  </footer>
+)
+
 const Page = () => (
-  <div>
-      <img src="react-logo.png" alt="react logo" width="80px" />
-      <h1>Fun facts about React</h1>
-      <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100K stars on GitHub</li>
-          <li>Is maintained by Facebook</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-  </div>
+  <page>
+    <Header />
+    <h1>Fun facts about React</h1>
+    <ol>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on GitHub</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ol>
+    <Footer />
+  </page>
 )
 
 root.render(<Page />)
